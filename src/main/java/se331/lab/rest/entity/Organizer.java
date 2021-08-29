c8752fe33a49b2ed2c2ebd3f45c62f5f00de564c
munlikaCh <munlika_ch@cmu.ac.th>
2021-08-29 16:40:40 +0700

    5.6

--- src/main/java/se331/lab/rest/entity/Organizer.java
+++ src/main/java/se331/lab/rest/entity/Organizer.java
@@ -1,4 +1,13 @@
 package se331.lab.rest.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+
 public class Organizer {
+    Long idorgan;
+    String name;
+    String address;
 }
